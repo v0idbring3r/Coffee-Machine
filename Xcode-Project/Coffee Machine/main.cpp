c622126c83b923496dfdb818fef58316045f6b20
abc <abc@xyz.com>
2020-06-24 15:53:06 +0530

    jsoncpp

--- Xcode-Project/Coffee Machine/main.cpp
+++ Xcode-Project/Coffee Machine/main.cpp
@@ -7,9 +7,22 @@
 //
 
 #include <iostream>
+#include "json/json.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    const std::string rawJson = R"({"Age": 20, "Name": "colin"})";
+    const auto rawJsonLength = static_cast<int>(rawJson.length());
+    JSONCPP_STRING err;
+    Json::Value root;
+    Json::CharReaderBuilder builder;
+    const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+    if (!reader->parse(rawJson.c_str(), rawJson.c_str() + rawJsonLength, &root,
+                       &err)) {
+      std::cout << "error" << std::endl;
+      return EXIT_FAILURE;
+    }
+    
+    
     return 0;
 }
